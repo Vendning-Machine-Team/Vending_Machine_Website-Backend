6efdb7999bd3bc4f5de29dc068360aeab321e7a3
SamMac55 <samanthamac055@gmail.com>
2026-04-07 15:43:10 -0500

    update schema to include image urls

--- data/schema.sql
+++ data/schema.sql
@@ -1,8 +1,9 @@
 CREATE TABLE products(
-        product_id INTEGER PRIMARY KEY,
+    product_id INTEGER PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     price DECIMAL (10,2) NOT NULL,
-    inventory INT DEFAULT 0 NOT NULL
+    inventory INT DEFAULT 0 NOT NULL,
+    image_url TEXT DEFAULT "../images/default.jpeg"
 );
 CREATE TABLE action_type(
     type_id INTEGER PRIMARY KEY,
